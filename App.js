c4e45a8884346fe11532b207db3430453132be9a
Csaba Koncz <ckoncz@cloudera.com>
2020-10-30 08:56:34 +0100

    Almost initial commit

---
+++ App.js
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, TextInput, View } from 'react-native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin';
+
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [text, setText] = useState('');
+  const [loggedIn, setloggedIn] = useState(false);
+  const [userInfo, setuserInfo] = useState([]);
+
+  _signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const {accessToken, idToken} = await GoogleSignin.signIn();
+      setloggedIn(true);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+        alert('Cancel');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        alert('Signin in progress');
+        // operation (f.e. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        alert('PLAY_SERVICES_NOT_AVAILABLE');
+        // play services not available or outdated
+      } else {
+        alert(JSON.stringify(error,null,2))
+      }
+    }
+  };
+
+  signOut = async () => {
+    try {
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      setloggedIn(false);
+      setuserInfo([]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    GoogleSignin.configure(
+      {
+        webClientId:'64702379780-t9a9uobgv383n0p9l29kmoqhdggs1j02.apps.googleusercontent.com'
+      }
+    )
+    fetch('https://reactnative.dev/movies.json')
+      .then((response) => response.json())
+      .then((json) => setData(json.movies))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator /> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}, {item.releaseYear}</Text>
+          )}
+        />
+      )}
+                <View>
+            <View>
+              <GoogleSigninButton
+                style={{width: 192, height: 48}}
+                size={GoogleSigninButton.Size.Wide}
+                color={GoogleSigninButton.Color.Dark}
+                onPress={this._signIn}
+              />
+            </View>
+            <View>
+              {!loggedIn && <Text>You are currently logged out</Text>}
+              {loggedIn && (
+                <Button
+                  onPress={this.signOut}
+                  title="LogOut"
+                  color="red"></Button>
+              )}
+            </View>
+          </View>
+      <TextInput
+        style={{ height: 40 }}
+        placeholder="Type here to translate!"
+        onChangeText={text => {
+          setText(text)
+          fetch('http://192.168.1.74:5000/parse/classes/GameScore', {
+            method: 'POST',
+            headers: {
+              'X-Parse-Application-Id': 'ftg-2020',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              "score": 100,
+              playerName: text,
+              cheatMode: true
+            })
+          }).catch(e => console.error(e))
+        }
+        }
+        defaultValue={text}
+      />
+      <Text style={{ padding: 10, fontSize: 42 }}>
+        {text.split(' ').map((word) => word && '🍕').join(' ')}
+      </Text>
+    </View>
+  );
+};
